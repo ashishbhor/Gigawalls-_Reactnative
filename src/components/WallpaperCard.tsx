9e20d4c1a724fced5d601a1eaa9501a28822b5fe
ashishbhor <bhorv590@gmail.com>
2026-01-13 20:18:35 +0530

    refactor: improve wallpaper card preview and download interaction

--- src/components/WallpaperCard.tsx
+++ src/components/WallpaperCard.tsx
@@ -98,7 +98,7 @@ export default function WallpaperCard({ wallpaper }: Props) {
             "
           >
             <Download size={14} />
-            HD
+            
           </a>
         </div>
       )}
